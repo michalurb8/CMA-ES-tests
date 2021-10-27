24e5da55b2524270a4b18d5aa1acd6df76b8b5ee
Michal Urbanski <michalurb8@wp.pl>
2021-10-27 21:04:17 +0200

    Add -s parameter to control stop_after

--- main.py
+++ main.py
@@ -13,9 +13,12 @@ parser.add_argument('-d', '--dimensions', type=int, default=10,
 parser.add_argument('-l', '--lbd', type=int, default=None,
                     help='Population size.')
 
+parser.add_argument('-s', '--stop', type=int, default=50,
+                    help='How many iterations to take average of.')
+
 parser.add_argument('-v', '--vis', default=False,
                     help='Turn on visualisation.', action='store_true')
 
 if __name__ == '__main__':
     args = parser.parse_args()
-    Evaluator.run_test(args.dimensions, args.iterations, args.lbd, args.vis)
+    Evaluator.run_test(args.dimensions, args.iterations, args.lbd, args.stop, args.vis)
