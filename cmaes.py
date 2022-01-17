0689ce5e0057b1124cbc538887ea735df41da1ad
Michal Urbanski <michalurb8@wp.pl>
2022-01-17 01:01:38 +0100

    back to constant 0.5

--- cmaes.py
+++ cmaes.py
@@ -167,7 +167,7 @@ class CMAES:
             # Delta 2: difference between selected generated points mean and selected repaired points mean:
             delta2 = (np.mean(originals[:self._mu], axis=0) - np.mean(population[:self._mu], axis=0)) / (self._sigma + _EPS)
 
-            alpha = 0.2 * repair_count/self._lambda
+            alpha = 0.5 * repair_count/self._lambda
             delta1_scaled = _resize(delta1, y_w, alpha)
             delta2_scaled = _resize(delta2, y_w, alpha)
 
